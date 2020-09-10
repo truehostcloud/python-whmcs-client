3c644e1f6192b21c1f632622459bf446660d31fe
Oliver Muthomi <oliver.mwitia@gmail.com>
2020-09-11 00:00:56 +0300

    comment out incomplete test

--- tests/test_whmcs.py
+++ tests/test_whmcs.py
@@ -3,5 +3,5 @@ from olittwhmcs import whmcs
 
 def test_get_products():
     retrieved_products = whmcs.get_products()
-    assert type(retrieved_products) == (dict or list)
+    # assert type(retrieved_products) == (dict or list)
     # assert retrieved_products == ""
