4a3cfea43547527f578d63bc350fe1dadcb0d479
Oliver Muthomi <oliver.mwitia@gmail.com>
2020-09-11 13:06:27 +0300

    enhance -- extract products array from whmcs reponse

--- olittwhmcs/whmcs.py
+++ olittwhmcs/whmcs.py
@@ -5,6 +5,8 @@ from olittwhmcs import network
 from olittwhmcs import serializer
 from olittwhmcs.exceptions import WhmcsException
 from olittwhmcs.models import Product
+from olittwhmcs.network import get_whmcs_response
+from olittwhmcs.serializer import get_product_request_parameters
 
 
 def get_products(group_id=None, module=None, product_ids=None):
@@ -17,13 +19,18 @@ def get_products(group_id=None, module=None, product_ids=None):
     :rtype: list
     :raises WhmcsException: If an error occurs.
     """
-    parameters = serializer.get_product_request_parameters(group_id, module, product_ids)
-    is_successful, response_or_error = network.get_whmcs_response(parameters)
+    parameters = get_product_request_parameters(group_id, module, product_ids)
+    is_successful, response_or_error = get_whmcs_response(parameters)
     if is_successful:
-        return response_or_error
-        products = []
-        for product in response_or_error:
-            products.append(Product(product))
-            return products
+        if response_or_error:
+            whmcs_products_wrapper = response_or_error.get('products', {})
+            whmcs_products = whmcs_products_wrapper.get('product', [])
+
+            return whmcs_products
+
+            products = []
+            for product in response_or_error:
+                products.append(Product(product))
+                return products
     default_error = "Unable to fetch products"
     raise WhmcsException(response_or_error if response_or_error else default_error)
