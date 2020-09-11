43ba7bef4928b0a5f60a5f4c4e99d44ab0160897
Oliver Muthomi <oliver.mwitia@gmail.com>
2020-09-11 16:12:36 +0300

    enhance -- generate product order parameters

--- olittwhmcs/serializer.py
+++ olittwhmcs/serializer.py
@@ -29,3 +29,36 @@ def get_product_request_parameters(group_id=None, module=None, product_ids=None)
     if product_ids:
         parameters.update({'pid': ','.join(map(str, product_ids))})
     return parameters
+
+
+def order_request_parameters(client_id, payment_method, billing_cycle, **kwargs):
+    parameters = get_default_parameters()
+    parameters.update({
+        'action': 'AddOrder',
+        'clientid': str(client_id),
+        'paymentmethod': payment_method,
+        'billingcycle': billing_cycle
+    })
+    for param, value in kwargs.items():
+        print(param)
+        print(value)
+        if param == 'price':
+            parameters.update({'priceoverride': value})
+        if param == 'promo_code':
+            parameters.update({'promocode': value})
+        if param == 'affiliate_id':
+            parameters.update({'affid': value})
+    return parameters
+
+
+def order_product_request_parameters(client_id, payment_method, billing_cycle, product_id, **kwargs):
+    """
+    Retrieve parameters for the product order request.
+    :param client_id: Integer, .
+    :param payment_method: String, .
+    :param billing_cycle: String, .
+    :param product_id: Integer, .
+    """
+    parameters = order_request_parameters(client_id, payment_method, billing_cycle, **kwargs)
+    parameters.update({'pid': product_id})
+    return parameters
--- olittwhmcs/whmcs.py
+++ olittwhmcs/whmcs.py
@@ -3,12 +3,17 @@
 from olittwhmcs.exceptions import WhmcsException
 from olittwhmcs.models import Product
 from olittwhmcs.network import get_whmcs_response
-from olittwhmcs.serializer import get_product_request_parameters
+from olittwhmcs.serializer import get_product_request_parameters, order_product_request_parameters
 
+
+############
+# PRODUCTS #
+############
 
 def get_products(currency, group_id=None, module=None, product_ids=None):
     """
     Retrieve products from WHMCS.
+    :param currency: String, currency in which to display prices. Eg, kes, usd...
     :param group_id: (Optional) Integer, id of the group from which to fetch products. Omit for all groups.
     :param module: (Optional) String, name of the module from which to fetch products. Omit for all modules.
     :param product_ids: (Optional) Integer array, list of product ids to retrieve.
@@ -28,3 +33,33 @@ def get_products(currency, group_id=None, module=None, product_ids=None):
         return products
     default_error = "Unable to fetch products"
     raise WhmcsException(response_or_error if response_or_error else default_error)
+
+
+def order_product(client_id, product_id, payment_method, billing_cycle, **kwargs):
+    """
+    Retrieve products from WHMCS.
+    :param currency: String, currency in which to display prices. Eg, kes, usd...
+    :param group_id: (Optional) Integer, id of the group from which to fetch products. Omit for all groups.
+    :param module: (Optional) String, name of the module from which to fetch products. Omit for all modules.
+    :param product_ids: (Optional) Integer array, list of product ids to retrieve.
+    :return: products retrieved from whmcs
+    :rtype: list
+    :raises WhmcsException: If an error occurs.
+    """
+    parameters = order_product_request_parameters(client_id, product_id, payment_method, billing_cycle, **kwargs)
+    return parameters
+    # is_successful, response_or_error = get_whmcs_response(parameters)
+    # if is_successful and response_or_error:
+    #     products = []
+    #     whmcs_products_wrapper = response_or_error.get('products', {})
+    #     whmcs_products = whmcs_products_wrapper.get('product', [])
+    #     for whmcs_product in whmcs_products:
+    #         product = Product(whmcs_product, currency)
+    #         products.append(product)
+    #     return products
+    # default_error = "Unable to fetch products"
+    # raise WhmcsException(response_or_error if response_or_error else default_error)
+
+############
+# ORDERS #
+############
