da9c38e26918544f99d06fc942de474bf924cc0e
Oliver Muthomi <oliver.mwitia@gmail.com>
2020-09-11 16:42:37 +0300

    enhance -- return order id and invoice id after placing a product order

--- olittwhmcs/whmcs.py
+++ olittwhmcs/whmcs.py
@@ -37,29 +37,22 @@ def get_products(currency, group_id=None, module=None, product_ids=None):
 
 def order_product(client_id, product_id, payment_method, billing_cycle, **kwargs):
     """
-    Retrieve products from WHMCS.
-    :param currency: String, currency in which to display prices. Eg, kes, usd...
-    :param group_id: (Optional) Integer, id of the group from which to fetch products. Omit for all groups.
-    :param module: (Optional) String, name of the module from which to fetch products. Omit for all modules.
-    :param product_ids: (Optional) Integer array, list of product ids to retrieve.
-    :return: products retrieved from whmcs
-    :rtype: list
-    :raises WhmcsException: If an error occurs.
+    Place a product order in WHMCS.
+    :param client_id: Integer, id of the client placing the order.
+    :param product_id: Integer, id of the product to order.
+    :param payment_method: String, preferred method of paying for the order.
+        Eg, paypal, rave, ...
+    :param billing_cycle: String, billing cycle. Eg, monthly, annually
+    :param kwargs: (Optional) Other parameters to add to the order payload.
+        Eg promo_code, affiliate_id, price (override), ...
+    :return: id of placed order, id of corresponding invoice
+    :rtype: int, int
     """
     parameters = order_product_request_parameters(client_id, product_id, payment_method, billing_cycle, **kwargs)
-    return parameters
-    # is_successful, response_or_error = get_whmcs_response(parameters)
-    # if is_successful and response_or_error:
-    #     products = []
-    #     whmcs_products_wrapper = response_or_error.get('products', {})
-    #     whmcs_products = whmcs_products_wrapper.get('product', [])
-    #     for whmcs_product in whmcs_products:
-    #         product = Product(whmcs_product, currency)
-    #         products.append(product)
-    #     return products
-    # default_error = "Unable to fetch products"
-    # raise WhmcsException(response_or_error if response_or_error else default_error)
-
-############
-# ORDERS #
-############
+    is_successful, response_or_error = get_whmcs_response(parameters)
+    if is_successful and response_or_error:
+        order_id = response_or_error.get('orderid')
+        invoice_id = response_or_error.get('invoiceid')
+        return order_id, invoice_id
+    default_error = "Unable to fetch products"
+    raise WhmcsException(response_or_error if response_or_error else default_error)
