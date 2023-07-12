aebeceef78909b82722ba7645086f005ee8aa4d5
Collins Lagat <cklagat@gmail.com>
2023-07-12 15:28:03 +0300

    feat: get base_url from env

--- olittwhmcs/settings.py
+++ olittwhmcs/settings.py
@@ -1,2 +1,4 @@
-WHMCS_BASE_URL = "https://www.olitt.com/billing/includes/api.php"
+import os
+
+WHMCS_BASE_URL = os.environ.get("WHMCS_BASE_URL", "https://www.olitt.com/billing")
 SECRET_KEY = "foobar"
